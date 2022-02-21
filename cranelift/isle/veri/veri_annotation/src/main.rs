d5a3ef58f8c513a5d5ddbcc0646e1bd1d8ffb979
Monica Pardeshi <monica.pardeshi@gmail.com>
2022-02-20 19:34:08 -0800

    make it compile

--- cranelift/isle/veri/veri_annotation/src/main.rs
+++ cranelift/isle/veri/veri_annotation/src/main.rs
@@ -1,8 +1,11 @@
-#[macro_use] extern crate lalrpop_util;
-
-lalrpop_mod!(pub parser);
+pub mod parser;
 
 #[test]
 fn parser() {
     assert!(parser::TypeParser::new().parse("bool").is_ok());
+    assert!(parser::TypeParser::new().parse("bv8").is_ok());
+}
+
+fn main() {
+    println!("Running tests..."); 
 }
