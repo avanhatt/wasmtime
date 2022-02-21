7644e9d0d762e49a0af9272eca1346cf06a0ea8d
Monica Pardeshi <monica.pardeshi@gmail.com>
2022-02-21 12:12:09 -0800

    add ret definition

--- cranelift/isle/veri/veri_annotation/src/main.rs
+++ cranelift/isle/veri/veri_annotation/src/main.rs
@@ -4,6 +4,8 @@ pub mod parser;
 fn parser() {
     assert!(parser::TypeParser::new().parse("bool").is_ok());
     assert!(parser::TypeParser::new().parse("bv8").is_ok());
+    assert!(parser::RetParser::new().parse("bool").is_ok());
+    assert!(parser::RetParser::new().parse("bv20").is_ok());
 }
 
 fn main() {
