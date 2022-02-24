7bf663d1dc269c91a13dc5c63c60c64ac3a36c81
Monica Pardeshi <monica.pardeshi@gmail.com>
2022-02-23 19:52:02 -0800

    add more tests

--- cranelift/isle/veri/veri_annotation/src/main.rs
+++ cranelift/isle/veri/veri_annotation/src/main.rs
@@ -2,10 +2,34 @@ pub mod parser;
 
 #[test]
 fn parser() {
-    assert!(parser::TypeParser::new().parse("bool").is_ok());
-    assert!(parser::TypeParser::new().parse("bv8").is_ok());
-    assert!(parser::RetParser::new().parse("bool").is_ok());
-    assert!(parser::RetParser::new().parse("bv20").is_ok());
+    // types
+    assert!(parser::VIRTypeParser::new().parse("bool").is_ok());
+    assert!(parser::VIRTypeParser::new().parse("bv8").is_ok());
+    // TODO: add IsleType
+
+    // bound var
+    assert!(parser::BoundVarParser::new().parse("b: bv16").is_ok());
+    assert!(parser::BoundVarParser::new().parse("arg: bool").is_ok());
+    assert!(parser::BoundVarParser::new().parse("ba: bv16").is_ok());
+    assert!(parser::BoundVarParser::new().parse("bv: bv32").is_err());
+
+    // function annotation
+    assert!(parser::FunctionAnnotationParser::new()
+        .parse("(sig (args) (ret: bool))").is_ok());
+    assert!(parser::FunctionAnnotationParser::new()
+        .parse("(sig (args a: bool) (ret: bool))").is_ok());
+    assert!(parser::FunctionAnnotationParser::new()
+        .parse("(sig (args a: bool, b: bv16) (ret: bool))").is_ok());
+
+    /*
+    let r = parser::FunctionAnnotationParser::new().parse("(sig (args a: bool, b: bv16) (ret: bool))");
+    match r {
+        Ok(_) => println!("woohoo!"),
+        Err(err) => panic!("{:?}", err),
+    };
+    */
+
+    // TODO: add assertions, compare fields in real annotations
 }
 
 fn main() {
