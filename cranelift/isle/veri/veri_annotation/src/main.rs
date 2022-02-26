a0df1aa7b90b75b230d93c58245a01c1b041d4de
Monica Pardeshi <monica.pardeshi@gmail.com>
2022-02-25 19:26:56 -0800

    Ignore partial commit. Just sharing progress.

--- cranelift/isle/veri/veri_annotation/src/main.rs
+++ cranelift/isle/veri/veri_annotation/src/main.rs
@@ -5,12 +5,12 @@ fn parser() {
     // types
     assert!(parser::VIRTypeParser::new().parse("bool").is_ok());
     assert!(parser::VIRTypeParser::new().parse("bv8").is_ok());
-    // TODO: add IsleType
+    assert!(parser::VIRTypeParser::new().parse("IsleType").is_ok());
 
     // bound var
     assert!(parser::BoundVarParser::new().parse("b: bv16").is_ok());
     assert!(parser::BoundVarParser::new().parse("arg: bool").is_ok());
-    assert!(parser::BoundVarParser::new().parse("ba: bv16").is_ok());
+    assert!(parser::BoundVarParser::new().parse("ba: IsleType").is_ok());
     assert!(parser::BoundVarParser::new().parse("bv: bv32").is_err());
 
     // function annotation
@@ -20,6 +20,11 @@ fn parser() {
         .parse("(sig (args a: bool) (ret: bool))").is_ok());
     assert!(parser::FunctionAnnotationParser::new()
         .parse("(sig (args a: bool, b: bv16) (ret: bool))").is_ok());
+
+    // bool expr
+    assert!(parser::BoolExprParser::new().parse("(True)").is_ok());
+    assert!(parser::BoolExprParser::new().parse("(False)").is_ok());
+    assert!(parser::BoolExprParser::new().parse("(Not (False))").is_ok());
 
     /*
     let r = parser::FunctionAnnotationParser::new().parse("(sig (args a: bool, b: bv16) (ret: bool))");
