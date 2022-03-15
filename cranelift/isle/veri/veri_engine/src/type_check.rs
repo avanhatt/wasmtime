388e25766c3f5acd66f6b174bffbe4ecddad6f45
Alexa VanHattum <amv94@cornell.edu>
2022-03-15 12:37:08 -0400

    renaming, more readme

--- cranelift/isle/veri/veri_engine/src/type_check.rs
+++ cranelift/isle/veri/veri_engine/src/type_check.rs
@@ -4,6 +4,10 @@ use crate::isle_annotations::isle_annotation_for_term;
 use cranelift_isle as isle;
 use isle::sema::{TypeEnv, TypeId};
 
+struct TypeContext {
+    
+}
+
 /// Get the Clif ISLE type name
 pub fn clif_type_name(typeid: TypeId, typeenv: &TypeEnv) -> String {
     match &typeenv.types[typeid.index()] {
