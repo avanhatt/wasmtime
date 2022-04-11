0a9849c3bd2605efa3c34c0f61e3224573d92339
Alexa VanHattum <amv94@cornell.edu>
2022-04-11 11:32:49 -0600

    comments

--- cranelift/isle/veri/veri_engine/src/solver.rs
+++ cranelift/isle/veri/veri_engine/src/solver.rs
@@ -261,6 +261,13 @@ pub fn run_solver_single_rule(
     }
 }
 
+/// Overall query:
+/// <declare vars>
+/// (not (=> (and 
+///             <all rules' assumptions>
+///             <between rule assumptions>
+///             <all but first rule's <LHS> = <RHS>>) 
+///          (= <first rule LHS> <first rule RHS>))))))
 pub fn run_solver_rule_path(rule_path: RulePath) -> VerificationResult {
     let mut solver = Solver::default_z3(()).unwrap();
 
