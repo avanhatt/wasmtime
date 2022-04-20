1de50c568e3ec7b774a85e006ebd5888b5b18ca3
mlfbrown <mlfbrown@stanford.edu>
2022-04-20 11:15:53 -0400

    Revert "ckpt"
    This reverts commit bfb577265dcbd5731b36a6edec028228f5b8738c.

--- cranelift/isle/veri/veri_engine/src/lib.rs
+++ cranelift/isle/veri/veri_engine/src/lib.rs
@@ -1,16 +1,3 @@
-//use crate::rule_tree::{verify_rules_with_lhs_root};
-use clap::{Arg, Command};
-use cranelift_isle as isle;
-use isle::sema::{Pattern, TermEnv, TypeEnv};
-use std::env;
-use std::path::PathBuf;
-use veri_annotation::parser_wrapper::parse_annotations;
-
-mod interp;
-mod renaming;
-mod rule_tree;
-mod solver;
-mod type_check;
 
 fn foo () {
     panic!()
