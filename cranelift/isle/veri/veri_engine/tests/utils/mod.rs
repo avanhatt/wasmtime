e516d8d2ac6a39e87db2a9904a7117ed008b33d2
mlfbrown <mlfbrown@stanford.edu>
2022-04-20 15:41:27 -0400

    almost to zero

--- cranelift/isle/veri/veri_engine/tests/utils/mod.rs
+++ cranelift/isle/veri/veri_engine/tests/utils/mod.rs
@@ -1,13 +1,13 @@
 use veri_engine_lib::interp::AssumptionContext;
 use veri_engine_lib::rule_tree::verify_rules_for_type_with_lhs_root;
 use veri_engine_lib::solver::run_solver_single_rule;
+use veri_engine_lib::parse_isle_to_terms;
 use cranelift_isle as isle;
 use isle::sema::{Rule, TermEnv, TypeEnv};
 use veri_annotation::parser_wrapper::{parse_annotations_str, AnnotationEnv};
 use veri_ir::{all_starting_bitvectors, VIRType, VerificationResult};
 
-#[cfg(test)]
-fn verify_rule_for_type(
+pub fn verify_rule_for_type(
     rule: &Rule,
     termenv: &TermEnv,
     typeenv: &TypeEnv,
@@ -21,7 +21,8 @@ fn verify_rule_for_type(
     run_solver_single_rule(rule_semantics, ty)
 }
 
-
-pub fn foo () {
-    panic!()
+pub fn isle_str_to_terms(s: &str) -> (TermEnv, TypeEnv) {
+    let lexer = isle::lexer::Lexer::from_str(s, "input.isle").unwrap();
+    parse_isle_to_terms(lexer)
 }
+
