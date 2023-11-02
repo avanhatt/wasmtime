d674d129d4eb1ddc55cae439f88afe23a037da48
Michael McLoughlin <mmcloughlin@gmail.com>
2023-11-02 15:01:46 -0400

    remove more overrides: Inst ShiftOpAndAmt ExtendedValue

--- cranelift/isle/veri/veri_engine/src/type_inference.rs
+++ cranelift/isle/veri/veri_engine/src/type_inference.rs
@@ -1385,33 +1385,30 @@ fn add_isle_constraints(
 
     // Temporary: hard-coded overrides.
     let clif_to_ir_type_overrides = HashMap::from([
+        // TODO(mbm): model is bv12 but bv24 here
         (
             "Imm12".to_owned(),
             annotation_ir::Type::BitVectorWithWidth(24),
         ),
+        // TODO(mbm): model is bv64 but unspecified size here
         ("usize".to_owned(), annotation_ir::Type::BitVector),
+        // TODO(mbm): model is an enum but integer here
         ("OperandSize".to_owned(), annotation_ir::Type::Int),
+        // TODO(mbm): model is an enum but integer here
         (
             "IntCC".to_owned(),
             annotation_ir::Type::BitVectorWithWidth(8),
         ),
-        ("Inst".to_owned(), annotation_ir::Type::BitVector),
+        // TODO(mbm): model is bv64 but bv here
         ("Amode".to_owned(), annotation_ir::Type::BitVector),
-        ("Value".to_owned(), annotation_ir::Type::BitVector),
-        (
-            "ExtendedValue".to_owned(),
-            annotation_ir::Type::BitVectorWithWidth(67),
-        ),
-        ("ValueRegs".to_owned(), annotation_ir::Type::BitVector),
-        ("InstOutput".to_owned(), annotation_ir::Type::BitVector),
+        // TODO(mbm): model is enum but bv1 here
         (
             "ImmExtend".to_owned(),
             annotation_ir::Type::BitVectorWithWidth(1),
         ),
-        (
-            "ShiftOpAndAmt".to_owned(),
-            annotation_ir::Type::BitVectorWithWidth(16),
-        ),
+        // -----------------
+        ("ValueRegs".to_owned(), annotation_ir::Type::BitVector),
+        ("InstOutput".to_owned(), annotation_ir::Type::BitVector),
     ]);
 
     // Apply overrides.
