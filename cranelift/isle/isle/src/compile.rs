44b831743ca8cf6e9b8e0180e6f3e9ee818dab17
Michael McLoughlin <mcloughlin@cmu.edu>
2024-04-22 14:58:42 -0400

    expand_internal_extractors

--- cranelift/isle/isle/src/compile.rs
+++ cranelift/isle/isle/src/compile.rs
@@ -8,7 +8,8 @@ use crate::{ast, codegen, sema};
 /// Compile the given AST definitions into Rust source code.
 pub fn compile(defs: &ast::Defs, options: &codegen::CodegenOptions) -> Result<String, Errors> {
     let mut typeenv = sema::TypeEnv::from_ast(defs)?;
-    let termenv = sema::TermEnv::from_ast(&mut typeenv, defs)?;
+    let termenv =
+        sema::TermEnv::from_ast(&mut typeenv, defs, /*expand_internal_extractors*/ true)?;
     let terms = crate::overlap::check(&typeenv, &termenv)?;
     Ok(codegen::codegen(&typeenv, &termenv, &terms, options))
 }
