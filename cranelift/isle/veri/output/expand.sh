9a51c8f62724ad88fd9ef91280075d00a6960d04
Michael McLoughlin <mcloughlin@cmu.edu>
2024-05-17 00:47:06 -0400

    expand output

---
+++ cranelift/isle/veri/output/expand.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+function expand() {
+    cargo run --bin expand -- \
+        --codegen-crate-dir ../../codegen/ \
+        --work-dir /tmp \
+        --name aarch64 \
+        "$@"
+}
+
+expand \
+    --term-name sink_load_into_addr \
+    > output/sink_load_into_addr.out
+
+expand \
+    --term-name sink_load_into_addr \
+    --inline add_imm_to_addr \
+    > output/sink_load_into_addr_inline_add_imm_to_addr.out
+
+expand \
+    --term-name lower \
+    > output/lower.out
