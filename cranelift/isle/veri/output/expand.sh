0008d93755506a0ce540552c9a85c84ec20b9c54
Michael McLoughlin <mcloughlin@cmu.edu>
2024-06-07 02:10:26 -0400

    examples of x64 lower inlining

--- cranelift/isle/veri/output/expand.sh
+++ cranelift/isle/veri/output/expand.sh
@@ -6,44 +6,64 @@ function expand() {
     cargo run --bin expand -- \
         --codegen-crate-dir ../../codegen/ \
         --work-dir /tmp \
-        --name aarch64 \
         "$@"
 }
 
 rm -f output/*.out
 
 expand \
+    --name aarch64 \
     --term-name sink_load_into_addr \
     > output/sink_load_into_addr.out
 
 expand \
+    --name aarch64 \
     --term-name sink_load_into_addr \
     --inline add_imm_to_addr \
     > output/sink_load_into_addr_inline_add_imm_to_addr.out
 
 expand \
+    --name aarch64 \
     --term-name sink_load_into_addr \
     --inline add_imm_to_addr \
     --inline add_imm \
     > output/sink_load_into_addr_inline_add_imm_to_addr_add_imm.out
 
 expand \
+    --name aarch64 \
     --term-name sink_load_into_addr \
     --maximal-inlining \
     > output/sink_load_into_addr_maximal_inlining.out
 
 expand \
+    --name aarch64 \
     --term-name lower \
     > output/lower.out
 
 expand \
+    --name aarch64 \
     --term-name lower \
     --no-expand-internal-extractors \
     > output/lower_internal_extractors.out
 
 expand \
+    --name aarch64 \
     --term-name lower \
     --no-expand-internal-extractors \
     --maximal-inlining \
     --max-rules 3 \
     > output/lower_internal_extractors_maximal_inline_3.out
+
+expand \
+    --name x64 \
+    --term-name lower \
+    --no-expand-internal-extractors \
+    > output/x64_lower_internal_extractors.out
+
+expand \
+    --name x64 \
+    --term-name lower \
+    --no-expand-internal-extractors \
+    --inline to_amode_add \
+    --inline amode_imm_reg_reg_shift \
+    > output/x64_lower_internal_extractors_amode_inlining.out
